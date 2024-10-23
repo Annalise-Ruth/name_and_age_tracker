0e511ffec4490c34d93188da45936c660f4a80ac
Annie <annaliseruth.delacruz@gmail.com>
2024-10-23 20:01:19 +0800

    Fixed the printing on the first loop, and implemented a collector for the information

--- name_and_age_tracker.py
+++ name_and_age_tracker.py
@@ -5,18 +5,31 @@
 #Compose a ranking system for the ages that had been given by the user
 
 user_information = {}
+collection_information = []
 
 while True:
     while True:
         try:
             name = input("Please input your name here, letters only: ")
             if not name.isalpha():
-                raise ValueError("That's wrong! Letters only please.")
+               raise ValueError("That's wrong! Letters only please.")
             age = int(input("Please input your age here: "))
             if age < 1 or age > 120:
                 raise ValueError("That's wrong! Numbers only and it should be from 1 to 120 years of age.")
-            user_information.append({"name": name, "age": age})
+            user_information = {
+                "name": name,
+                "age": age
+            }
+            collection_information.append(user_information)
             break
-        except:
-            print("Error")
-    
+        except ValueError as value_error:
+            print(value_error)
+        except Exception as exception:
+            print(exception)
+
+    retry = input("Do you want to input another entry? (Yes/No): ")
+    if retry == "No":
+        break
+    elif retry == "Yes":
+        pass
+print(collection_information)
