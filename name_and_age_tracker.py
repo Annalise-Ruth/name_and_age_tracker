3403acf65d3b813a94aa24746c969d3067c4f298
Annie <annaliseruth.delacruz@gmail.com>
2024-10-23 20:24:27 +0800

    Fixed the Yes/No input

--- name_and_age_tracker.py
+++ name_and_age_tracker.py
@@ -2,34 +2,44 @@
 #Set a definition for the name and age input
 #Have a loop for re-entry of name and age
 #Have an array to collect the infomation given by the user
-#Compose a ranking system for the ages that had been given by the user
+#Compose a 
 
 user_information = {}
 collection_information = []
+retry = True
 
-while True:
-    while True:
-        try:
-            name = input("Please input your name here, letters only: ")
-            if not name.isalpha():
-               raise ValueError("That's wrong! Letters only please.")
-            age = int(input("Please input your age here: "))
-            if age < 1 or age > 120:
-                raise ValueError("That's wrong! Numbers only and it should be from 1 to 120 years of age.")
-            user_information = {
-                "name": name,
-                "age": age
-            }
-            collection_information.append(user_information)
-            break
+while retry:
+    try:
+        name = input("Please input your name here, letters only: ")
+        if not name.isalpha():
+            raise ValueError("That's wrong! Letters only please.")
+        age = int(input("Please input your age here: "))
+        if age < 1 or age > 120:
+            raise ValueError("That's wrong! Numbers only and it should be from 1 to 120 years of age.")
+        user_information = {
+            "name": name,
+            "age": age
+        }
+        collection_information.append(user_information)
+    except ValueError as value_error:
+        print(value_error)
+    except Exception as exception:
+        print(exception)
+
+    while True: 
+        try:  
+            retry_input = input("Do you want to input another entry? (Yes/No): ")
+            if retry_input == "No" or retry_input == "no":
+                retry = False
+                break
+            elif retry_input == "Yes" or retry_input == "yes":
+                break
+            else:
+                raise ValueError("Invalid")
         except ValueError as value_error:
             print(value_error)
         except Exception as exception:
             print(exception)
 
-    retry = input("Do you want to input another entry? (Yes/No): ")
-    if retry == "No":
-        break
-    elif retry == "Yes":
-        pass
+
 print(collection_information)
