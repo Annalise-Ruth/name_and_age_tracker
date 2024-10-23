a7accc900ca109eeafb3c7de8f3683dc783f1811
Annie <annaliseruth.delacruz@gmail.com>
2024-10-23 17:39:11 +0800

    Pseudocode done

---
+++ name_and_age_tracker.py
@@ -0,0 +1,5 @@
+#Ask user for name and age input
+#Set a definition for the name and age input
+#Have a loop for re-entry of name and age
+#Have an array to collect the infomation given by the user
+#Compose a ranking system for the ages that had been given by the user
